7f3b6560bc85cd26fc474b6026b6b6f921d6b43a
evelynlab <celayu@163.com>
2019-09-03 17:24:50 +0800

    feat: use hooks

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class CSSTreeShakingPlugin {
   }
 
   apply (compiler) {
-    compiler.plugin('after-emit', (compilation, callback) => {
+    compiler.hooks.emit.tapAsync('CSSTreeShakingPlugin', (compilation, callback) => {
       let styleFiles = Object.keys(compilation.assets).filter(asset => {
         return /\.css$/.test(asset)
       })
